77d6f97fb567a604c7d41fe9dcf0c699333722fa
Aman Verma <aman@Amans-MacBook-Air.local>
2024-05-12 21:33:56 +0530

    active color on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,47 @@
 "use client";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+
+import React from "react";
 
 function Navbar() {
-  const router = useRouter();
-  const { pathname } = router;
-
-  useEffect(() => {
-    console.log("AMAN", pathname);
-  });
+  const location = window.location.pathname;
 
   return (
     <div className=" hidden md:flex justify-end ">
       <div className="navbar bg-[#383839] px-5 py-3 rounded-bl-2xl rounded-tr-2xl w-[60%]">
         <ul className="flex items-center justify-around text-base font-medium p-2">
-          <Link href="/">
+          <Link
+            href="/"
+            className={`${location === "/" ? "text-yellow-primary" : ""}`}
+          >
             <li className="hover:text-text-secondary transition-all ease-in-out duration-200">
               About
             </li>
           </Link>
-          <Link href={"/resume"}>
+          <Link
+            href={"/resume"}
+            className={`${location === "/resume" ? "text-yellow-primary" : ""}`}
+          >
             <li className="hover:text-text-secondary transition-all ease-in-out duration-200">
               Resume
             </li>
           </Link>
-          <Link href={"/portfolio"}>
+          <Link
+            href={"/portfolio"}
+            className={`${
+              location === "/portfolio" ? "text-yellow-primary" : ""
+            }`}
+          >
             <li className="hover:text-text-secondary transition-all ease-in-out duration-200">
               Portfolio
             </li>
           </Link>
-          <Link href={"/contact"}>
+          <Link
+            href={"/contact"}
+            className={`${
+              location === "/contact" ? "text-yellow-primary" : ""
+            }`}
+          >
             <li className="hover:text-text-secondary transition-all ease-in-out duration-200">
               Contact
             </li>
