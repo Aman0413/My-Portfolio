3e528cc063fb669dcf65c440a1561be842726d73
Aman Verma <aman@Amans-MacBook-Air.local>
2025-01-10 15:18:56 +0530

    add smooth scrolling

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import React from "react";
 import { SiAffinitydesigner } from "react-icons/si";
 import { motion } from "framer-motion";
 import ShowcaseModal from "../components/ShowcaseModal";
+import { ReactLenis } from "../utils/lenis";
 
 function page() {
   const data = [
@@ -29,35 +30,37 @@ function page() {
       transition={{ duration: 0.5 }}
       className="about py-6"
     >
-      <div>
-        <div className="flex flex-col space-y-3 ">
-          <h1 className="text-3xl font-semibold">About Me</h1>
-          <div className="bg-yellow-primary w-10 h-1 rounded-2xl"></div>
-        </div>
-
-        <div className="mt-4 ">
-          <p className="text-text-secondary text-left leading-7 font-normal">
-            I&apos;m your full-stack MERN virtuoso, with Next.js as my secret
-            wand. With MongoDB, Express.js, React, Node.js, and the wizardry of
-            Next.js in my arsenal, I conjure up web applications that dazzle and
-            delight. From sleek interfaces to seamless server-side rendering, I
-            specialize in crafting experiences that leave users spellbound.
-            Let&apos;s collaborate to transform your visions into digital
-            enchantments.
-          </p>
-        </div>
-
-        <div className="mt-10 ">
-          <div className="text-2xl  font-semibold">
-            <h2>What I&apos;m doing</h2>
+      <ReactLenis root>
+        <div>
+          <div className="flex flex-col space-y-3 ">
+            <h1 className="text-3xl font-semibold">About Me</h1>
+            <div className="bg-yellow-primary w-10 h-1 rounded-2xl"></div>
           </div>
-          <div className="flex flex-wrap justify-between gap-2 mt-6 pb-10">
-            {data.map((item) => {
-              return <ShowcaseModal key={item.id} item={item} />;
-            })}
+
+          <div className="mt-4 ">
+            <p className="text-text-secondary text-left leading-7 font-normal">
+              I&apos;m your full-stack MERN virtuoso, with Next.js as my secret
+              wand. With MongoDB, Express.js, React, Node.js, and the wizardry
+              of Next.js in my arsenal, I conjure up web applications that
+              dazzle and delight. From sleek interfaces to seamless server-side
+              rendering, I specialize in crafting experiences that leave users
+              spellbound. Let&apos;s collaborate to transform your visions into
+              digital enchantments.
+            </p>
+          </div>
+
+          <div className="mt-10 ">
+            <div className="text-2xl  font-semibold">
+              <h2>What I&apos;m doing</h2>
+            </div>
+            <div className="flex flex-wrap justify-between gap-2 mt-6 pb-10">
+              {data.map((item) => {
+                return <ShowcaseModal key={item.id} item={item} />;
+              })}
+            </div>
           </div>
         </div>
-      </div>
+      </ReactLenis>
     </motion.div>
   );
 }
